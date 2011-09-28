87f19e2261269aeb211c808fc2bdeff2c921068b
Chris Keller <KellerUser@mactoppy.betahaus.lan>
2011-09-28 05:33:25 -0500

    Revert f5f69b7609a12de0ec82d1c811796a71d224df7e^..HEAD

--- scripts/new-query.js
+++ scripts/new-query.js
@@ -40,7 +40,6 @@ $(document).ready(function(){
 								'<a class="relationships_link" href="' + entity.id +'"><br><h3>' +
 								entity.name + '</h3></a>' + entity.description +
 								'<img src=\"images/anon.png\" width=\"252px\" />' +
-								//'<br>' +
 								'</li>');
               	});
             });
