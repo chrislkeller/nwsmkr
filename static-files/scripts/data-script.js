a44ef33f095d0ca368a0d84e004cec41c6862d99
chrislkeller <christopherlawrencekeller@gmail.com>
2013-07-21 11:44:20 -0700

    adds static-files directory

---
+++ static-files/scripts/data-script.js
@@ -0,0 +1,21 @@
+    var submitted = false;
+    var jqueryNoConflict = jQuery;
+
+    // begin main function
+    jqueryNoConflict(document).ready(function(){
+        retriveData();
+    });
+
+    // clear form fields on focus
+    function clickclear(thisfield, defaulttext) {
+        if (thisfield.value == defaulttext) {
+            thisfield.value = "";
+        }
+    }
+
+    // rewrite form fields on focus
+    function clickrecall(thisfield, defaulttext) {
+        if (thisfield.value == "") {
+            thisfield.value = defaulttext;
+        }
+    }
---
+++ static-files/scripts/template-scripts.js
@@ -0,0 +1,20 @@
+var jqueryNoConflict = jQuery;
+
+// begin main function
+jqueryNoConflict(document).ready(function() {
+    //renderHandlebarsTemplate('static-files/templates/page-header.handlebars', '#page-header');
+    renderHandlebarsTemplate('static-files/templates/project-details.handlebars', '#project-details');
+    renderHandlebarsTemplate('static-files/templates/project-visuals.handlebars', '#project-visuals');
+    renderHandlebarsTemplate('static-files/templates/project-footer.handlebars', '#project-footer');
+    //renderHandlebarsTemplate('static-files/templates/page-footer.handlebars', '#page-footer');
+});
+// end
+
+// add handlebars debugger
+function handlebarsDebugHelper(){
+    Handlebars.registerHelper("debug", function(optionalValue) {
+        console.log("Current Context");
+        console.log("====================");
+        console.log(this);
+    });
+};
